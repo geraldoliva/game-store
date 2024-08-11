9694e3c7426ccc57a155331bb9114a0cb58f7667
Gerald Oliva <geraldsamoliva@gmail.com>
2024-08-11 11:08:39 -0400

    Implement CRUD endpoints

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,11 @@
+using GameStore.Api.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGamesEndpoints();
+
+
 
 app.Run();
